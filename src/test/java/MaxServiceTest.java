6fc8ba7439df0de7126d5af296674ed7d35e8f26
GIT-ABYLAI <d.a.auken-00@mail.ru>
2023-09-28 23:21:39 +0600

    third commit

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -5,13 +5,25 @@ public class MaxServiceTest {
 
 
     @Test
-    public void shouldFindMax(){
+    public void shouldFindMax() {
         MaxService service = new MaxService();
 
         int a = 5;
-        int b =3;
-        int expected = 5;
-        int actual = service.max(a,b);
+        int b = 3;
+        int expected = a;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMaxIfBBiggerThanA() {
+        MaxService service = new MaxService();
+
+        int a = 3;
+        int b = 5;
+        int expected = b;
+        int actual = service.max(a, b);
 
         Assertions.assertEquals(expected, actual);
     }
